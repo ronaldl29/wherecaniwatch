34faa20c35c1d7a684d2dea4767cffe17787ab19
Ronald Long <ronaldlong@protonmail.com>
2020-11-25 21:41:21 -0800

    Init

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// @route    GET api/posts
+// @desc     Get all posts
+// @access   Private
+app.get('/', async (req, res) => {
+    try {
+        res.render("index");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
