fbae24df4ece422bf2d6771b23f87abeb645316d
Ronald Long <ronaldlong@protonmail.com>
2020-12-03 22:54:11 -0800

    Updated port

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.get('/', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
